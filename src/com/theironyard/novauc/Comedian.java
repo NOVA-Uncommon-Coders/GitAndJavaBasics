8a82a798a2f7245f0ab58352fa95a699f6212754
D'Angelo Joyce <dangelomjoyce@gmail.com>
2017-02-07 15:29:37 -0500

    Finally, I think I have it right.

---
+++ src/com/theironyard/novauc/Comedian.java
@@ -0,0 +1,14 @@
+package com.theironyard.novauc;
+
+/**
+ * Created by dangelojoyce on 2/7/17.
+ */
+public class Comedian {
+    int age;
+    int jokesNumber;
+    String name;
+    String jokes;
+    boolean laughs;
+
+
+}
---
+++ src/com/theironyard/novauc/Table.java
@@ -0,0 +1,15 @@
+package com.theironyard.novauc;
+
+/**
+ * Created by dangelojoyce on 2/7/17.
+ */
+public class Table {
+
+    int legs;
+    int screws;
+    String woodType;
+    boolean mahogany;
+    boolean oak
+
+
+}
