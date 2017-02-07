b2adc6c6977037de119cf010a465aa96b9d7e90b
JacobP <JacobP@macusermacbook6.nvstu.edu>
2017-02-07 14:04:01 -0500

    created 5 classes and added 5 variables to each

---
+++ src/com/theironyard/novauc/MovieList.java
@@ -0,0 +1,23 @@
+package com.theironyard.novauc;
+
+/**
+ * Created by JacobP on 2/7/17.
+ */
+public class MovieList {
+    String title;
+    char rating;
+    int year;
+    double runtime;
+    String director;
+    
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/com/theironyard/novauc/TravelCost.java
@@ -0,0 +1,22 @@
+package com.theironyard.novauc;
+
+/**
+ * Created by JacobP on 2/7/17.
+ */
+public class TravelCost {
+    int distance;
+    double mpg;
+    double priceperGallon;
+    double lodgingCost;
+    double totalCost;
+
+
+
+
+
+
+
+
+
+
+}
