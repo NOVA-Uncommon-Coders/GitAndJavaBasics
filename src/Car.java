0dfb0bedeec8b4eb8dbea38cd32e2d3cfb737b52
Dave Office <daveoffice@kazis-mbp.nvstu.edu>
2017-02-07 14:26:49 -0500

    commit2

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+/**
+ * Created by daveoffice on 2/7/17.
+ */
+public class Car {
+    String Make;
+    String model;
+    String color;
+    int year;
+    int class;
+}
---
+++ src/F35.java
@@ -0,0 +1,10 @@
+/**
+ * Created by daveoffice on 2/7/17.
+ */
+public class F35 {
+    String manufacturer;
+    String type;
+    int crew;
+    char desingation;
+    boolean isairplane;
+}
