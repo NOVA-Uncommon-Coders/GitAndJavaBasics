f8c79aafeb36e8b7b0645f78d109f232a970ba52
Ross Small <Merlin@rosss-mbp.nvstu.edu>
2017-02-07 14:29:17 -0500

    created 5 classes with 5 different variables for each

---
+++ src/com/theironyard/novauc/Computer.java
@@ -0,0 +1,12 @@
+package com.theironyard.novauc;
+
+/**
+ * Created by Merlin on 2/7/17.
+ */
+public class Computer {
+    String OperatingSystem;
+    String BuiltBy;
+    int RAMinGB;
+    int ProcessorSpeedinGHz;
+    boolean SolidStateDrive;
+}
---
+++ src/com/theironyard/novauc/Vehicle.java
@@ -0,0 +1,13 @@
+package com.theironyard.novauc;
+
+/**
+ * Created by Merlin on 2/7/17.
+ */
+public class Vehicle {
+    String make;
+    String model;
+    int year;
+    String color;
+    boolean isClassicCar;
+
+}
