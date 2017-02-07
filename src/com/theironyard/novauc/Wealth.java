aecc0f1769f727bfe0c547d48bfbde16275a54bd
Octavio Cisneros <octavio@octavios-air.nvstu.edu>
2017-02-07 15:13:23 -0500

    Added 5 Classes

---
+++ src/com/theironyard/novauc/Wealth.java
@@ -0,0 +1,17 @@
+package com.theironyard.novauc;
+
+/**
+ * Created by octavio on 2/7/17.
+ */
+public class Wealth {
+        String Occupation;
+        String Company;
+        int yearsEmployed;
+        int salary;
+        boolean loveJob;
+
+
+
+
+
+}
