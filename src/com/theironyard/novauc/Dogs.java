bbfcc1f2c06f48cc68bb3343584c501940949b02
Chris' Old Laptop <christinekoufos old@christines-mbp.nvstu.edu>
2017-02-07 14:48:46 -0500

    made 5 classes

---
+++ src/com/theironyard/novauc/Dogs.java
@@ -0,0 +1,15 @@
+package com.theironyard.novauc;
+
+/**
+ * Created by christinekoufos old on 2/7/17.
+ */
+public class Dogs {
+    String breed;
+    String color;
+    int weight;
+    int age;
+    boolean dryFood;
+    
+
+
+}
---
+++ src/com/theironyard/novauc/MyCar.java
@@ -0,0 +1,15 @@
+package com.theironyard.novauc;
+
+/**
+ * Created by christinekoufos old on 2/7/17.
+ */
+public class MyCar {
+    String brand;
+    String model;
+    int year;
+    String color;
+    boolean coupe;
+
+
+
+}
