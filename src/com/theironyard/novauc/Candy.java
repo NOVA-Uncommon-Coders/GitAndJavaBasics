34e4b59c679a1dbbdd51bcc5db8c968bdaca6ef0
Jeriesha Smith <jerieshasmith@jerieshas-mbp.nvstu.edu>
2017-02-07 14:29:20 -0500

    Git and Java Basics

---
+++ src/com/theironyard/novauc/Candy.java
@@ -0,0 +1,7 @@
+package com.theironyard.novauc;
+
+/**
+ * Created by jerieshasmith on 2/7/17.
+ */
+public class Candy {
+}
---
+++ src/com/theironyard/novauc/House.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jerieshasmith on 2/7/17.
+ */
+public class House {
+String material;
+String color;
+int address;
+int length
+boolean isBrick;
+
+
+}
